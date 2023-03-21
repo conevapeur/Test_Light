f7b7cfa3c943c014755287a7c4fbd190456a374b
Louis Soulié <93200607+conevapeur@users.noreply.github.com>
2023-03-21 22:25:52 +0100

    rangement

---
+++ Assets/UI/IReveal.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReveal
+{
+    public void Reveal();
+    public void Hide();
+    public void Switch();
+}
