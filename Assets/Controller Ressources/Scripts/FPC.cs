21d1347815af0be86fbda5e66bf05627382f95ab
laBeuhlette <axelcasanovas@gmail.com>
2023-03-28 14:36:16 +0200

    climb patch

--- Assets/Controller Ressources/Scripts/FPC.cs
+++ Assets/Controller Ressources/Scripts/FPC.cs
@@ -58,103 +58,106 @@ public class FPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        yRotation = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensivity;
-        //yRotation = transform.localEulerAngles.y + Input.GetAxis("Gamepad X") * mouseSensivity;
-        xRotation = xRotation - Input.GetAxis("Mouse Y") * mouseSensivity;
-        //xRotation = xRotation - Input.GetAxis("Gamepad Y") * mouseSensivity;
-
-        xRotation = Mathf.Clamp(xRotation, -maxLookAngle, maxLookAngle);
-
-        transform.localEulerAngles = new Vector3(0, yRotation, 0);
-        joint.transform.localEulerAngles = new Vector3(xRotation, 0, 0);
-
-
-        // sprint
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Joystick1Button8))
+        if(canMove)
         {
-            moveSpeed = 4f;
-        }
+            #region move
+            yRotation = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensivity;
+            //yRotation = transform.localEulerAngles.y + Input.GetAxis("Gamepad X") * mouseSensivity;
+            xRotation = xRotation - Input.GetAxis("Mouse Y") * mouseSensivity;
+            //xRotation = xRotation - Input.GetAxis("Gamepad Y") * mouseSensivity;
 
-        if (moveSpeed != 2f)
-        {
-            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            xRotation = Mathf.Clamp(xRotation, -maxLookAngle, maxLookAngle);
+
+            transform.localEulerAngles = new Vector3(0, yRotation, 0);
+            joint.transform.localEulerAngles = new Vector3(xRotation, 0, 0);
+
+
+            // sprint
+            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Joystick1Button8))
             {
-                moveSpeed = 2f;
+                moveSpeed = 4f;
             }
+
+            if (moveSpeed != 2f)
+            {
+                if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+                {
+                    moveSpeed = 2f;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                target = null;
+                rayLook();
+                interact(target);
+            }
+
+            #endregion
         }
+
 
         
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            target = null;
-            rayLook();
-            interact(target);
-        }
 
 
         
     }
     void FixedUpdate()
     {
-        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        targetVelocity = transform.TransformDirection(targetVelocity) * moveSpeed;
-
-        //Apply a force
-        Vector3 velocityChange = targetVelocity - rb.velocity;
-        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-        velocityChange.y = 0;
-        rb.AddForce(velocityChange, ForceMode.VelocityChange);
-
-
-        /*
-        Vector3 gravity = new Vector3(0, -9.81f, 0);
-        float gravityMultiplier = 5f;
-        gravity *= gravityMultiplier;
-
-        if (rb.velocity.y < 0)
+        if(canMove)
         {
-            rb.AddForce(gravity, ForceMode.Force);
-        }
-        */
+            #region move
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity) * moveSpeed;
 
-        RaycastHit hit;
+            //Apply a force
+            Vector3 velocityChange = targetVelocity - rb.velocity;
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
 
-        Vector3 rayDir = transform.TransformDirection(Vector3.down);
+            RaycastHit hit;
 
-        int layerMask = 1 << 6;
+            Vector3 rayDir = transform.TransformDirection(Vector3.down);
 
-        layerMask = ~layerMask;
-        if (Physics.Raycast(transform.position, rayDir, out hit, floatHeight, layerMask))
-        {
+            int layerMask = 1 << 6;
 
-            Vector3 velocity = rb.velocity;
-
-            Vector3 otherVelocity = Vector3.zero;
-
-            Rigidbody contactBody = hit.rigidbody;
-
-            if (contactBody != null)
+            layerMask = ~layerMask;
+            if (Physics.Raycast(transform.position, rayDir, out hit, floatHeight, layerMask))
             {
-                otherVelocity = contactBody.velocity;
+
+                Vector3 velocity = rb.velocity;
+
+                Vector3 otherVelocity = Vector3.zero;
+
+                Rigidbody contactBody = hit.rigidbody;
+
+                if (contactBody != null)
+                {
+                    otherVelocity = contactBody.velocity;
+                }
+
+                float rayDirVelocity = Vector3.Dot(rayDir, velocity);
+                float otherDirVelocity = Vector3.Dot(rayDir, otherVelocity);
+
+                float relativeVelocity = rayDirVelocity - otherDirVelocity;
+
+                float x = hit.distance - floatHeight;
+
+                float springForce = (x * floatForce) - (relativeVelocity * floatSpringDamper);
+
+                rb.AddForce(rayDir * springForce);
+
+                if (contactBody != null)
+                {
+                    contactBody.AddForceAtPosition(rayDir * -springForce, hit.point);
+                }
             }
 
-            float rayDirVelocity = Vector3.Dot(rayDir, velocity);
-            float otherDirVelocity = Vector3.Dot(rayDir, otherVelocity);
-
-            float relativeVelocity = rayDirVelocity - otherDirVelocity;
-
-            float x = hit.distance - floatHeight;
-
-            float springForce = (x * floatForce) - (relativeVelocity * floatSpringDamper);
-
-            rb.AddForce(rayDir * springForce);
-
-            if (contactBody != null)
-            {
-                contactBody.AddForceAtPosition(rayDir * -springForce, hit.point);
-            }
+            #endregion
         }
+
 
     }
 
@@ -214,6 +217,7 @@ public class FPC : MonoBehaviour
     {
         canMove = false;
         rb.useGravity = false;
+        rb.velocity = new Vector3(0,0,0);
         
         Transform mimic = target.transform.GetChild(0);
         Vector3 targetPos = new Vector3(0,0,0);
