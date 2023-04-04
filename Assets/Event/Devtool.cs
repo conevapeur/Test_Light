51293f9e4e05f19a61ff44a3a200259b5ea52d02
Louis Soulié <93200607+conevapeur@users.noreply.github.com>
2023-04-04 22:23:03 +0200

    devtool mdr

--- Assets/Event/Devtool.cs
+++ Assets/Event/Devtool.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 
 public class Devtool : MonoBehaviour
 {
-    public bool flag;
-    public int i = 1;
-    public GameObject _gameObject;
+    public bool triggerAnim;
+    public Animator _animator;
+
+    public bool triggerSound;
+    public AudioSource _audioSource;
+
+    public bool triggerDebug;
+    public string _debug;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(_debug);
+        }
+    }
 }
 
-[CustomEditor(typeof(Devtool))]
+[CustomEditor(typeof(Devtool))] 
 public class DevtoolEditor :  Editor
 {
     override public void OnInspectorGUI()
     {
         var myDevtool = target as Devtool;
+       
 
-        myDevtool.flag = GUILayout.Toggle(myDevtool.flag, "Flag");
+        EditorGUILayout.LabelField("Animation", EditorStyles.boldLabel); // header
 
-        if (myDevtool.flag)
-            myDevtool.i = EditorGUILayout.IntSlider("I field:", myDevtool.i, 1, 100);
-            //myDevtool._gameObject = EditorGUILayout.ObjectField("gameObject",myDevtool._gameObject, typeof(Object));
+        myDevtool.triggerAnim = GUILayout.Toggle(myDevtool.triggerAnim, "Trigger Animation ?"); // switch
+        if (myDevtool.triggerAnim)
+            myDevtool._animator = (Animator)EditorGUILayout.ObjectField("Animator", myDevtool._animator, typeof(Animator), true); // cast type Object en Animator
+
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Sound Effect", EditorStyles.boldLabel);
+
+        myDevtool.triggerSound = GUILayout.Toggle(myDevtool.triggerSound, "Trigger Sound Effect ?");
+        if (myDevtool.triggerSound)
+            myDevtool._audioSource = (AudioSource)EditorGUILayout.ObjectField("AudioSource",myDevtool._audioSource, typeof(AudioSource), true);
+
+
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Debug", EditorStyles.boldLabel);
+
+        myDevtool.triggerDebug = GUILayout.Toggle(myDevtool.triggerDebug, "Debug ?");
+        if (myDevtool.triggerDebug)
+            myDevtool._debug = EditorGUILayout.TextField("Chien");
+
+
+
 
     }
 }
