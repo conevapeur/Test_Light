e96455754d0793c1a951f5810c458e0e8683ccf7
Nathan Le Gac <93200607+conevapeur@users.noreply.github.com>
2023-04-05 12:27:49 +0200

    event

--- Assets/Event/Devtool.cs
+++ Assets/Event/Devtool.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Devtool : MonoBehaviour
 {
@@ -14,12 +16,14 @@ public class Devtool : MonoBehaviour
     public bool triggerDebug;
     public string _debug;
 
-    private void OnTriggerEnter(Collider other)
+    public bool triggerEvent;
+    public UnityEvent _event;
+   
+
+
+    public void DoSomething()
     {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log(_debug);
-        }
+        Debug.Log(_debug);
     }
 }
 
@@ -30,7 +34,6 @@ public class DevtoolEditor :  Editor
     {
         var myDevtool = target as Devtool;
        
-
         EditorGUILayout.LabelField("Animation", EditorStyles.boldLabel); // header
 
         myDevtool.triggerAnim = GUILayout.Toggle(myDevtool.triggerAnim, "Trigger Animation ?"); // switch
@@ -44,16 +47,21 @@ public class DevtoolEditor :  Editor
         if (myDevtool.triggerSound)
             myDevtool._audioSource = (AudioSource)EditorGUILayout.ObjectField("AudioSource",myDevtool._audioSource, typeof(AudioSource), true);
 
-
         EditorGUILayout.Space(10);
         EditorGUILayout.LabelField("Debug", EditorStyles.boldLabel);
 
         myDevtool.triggerDebug = GUILayout.Toggle(myDevtool.triggerDebug, "Debug ?");
         if (myDevtool.triggerDebug)
-            myDevtool._debug = EditorGUILayout.TextField("Chien");
+            myDevtool._debug = EditorGUILayout.TextField(myDevtool._debug);
 
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Event", EditorStyles.boldLabel);
 
-
+        var _event = serializedObject.FindProperty("_event");
+        myDevtool.triggerEvent = GUILayout.Toggle(myDevtool.triggerEvent, "Event ?");
+        if (myDevtool.triggerEvent)
+            EditorGUILayout.PropertyField(_event, true);
+        serializedObject.ApplyModifiedProperties();
 
     }
 }
