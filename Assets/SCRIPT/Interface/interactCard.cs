ee0732da6944c293e8e6b5dd8a04258466e109f4
Louis Soulié <93200607+conevapeur@users.noreply.github.com>
2023-05-18 12:36:52 +0200

    clenage

--- Assets/SCRIPT/Interface/interactCard.cs
+++ Assets/SCRIPT/Interface/interactCard.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class interactCard: MonoBehaviour, IInteract
 {
     [SerializeField] private Animator _security;
+    [SerializeField] private Animator animatorCam;
 
+    private void Awake()
+    {
+        interactReadable.card = true;
+    }
     public void OnInteract()
     {
         Debug.Log(interactReadable.card);
+
+
         if (interactReadable.card == true)
         {
-            _security.SetTrigger("trigger");
+            StartCoroutine(cardCoroutine());
+        }
+
+        IEnumerator cardCoroutine()
+        {
+            
             GameManager.instance.player.GetComponent<FPC>().animator.SetTrigger("triggerCard");
+            animatorCam.SetTrigger("triggerCard");
+            interactReadable.card = false;
+            yield return new WaitForSeconds(2f);
+            _security.SetTrigger("trigger");
+            
+
         }
 
     }
