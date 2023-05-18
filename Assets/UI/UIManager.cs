180514f5d0d59757b9261f69337741c940f07cd3
Louis Soulié <93200607+conevapeur@users.noreply.github.com>
2023-05-18 12:51:49 +0200

    ui rectif

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -30,8 +30,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject noButton;
 
 
-    [Space (10)]
-    [Header("Readables")] 
+    [Space(10)]
+    [Header("Readables")]
     [SerializeField] private GameObject readable1;
     [SerializeField] private GameObject readable2;
     [SerializeField] private GameObject readable3;
@@ -56,7 +56,7 @@ public class UIManager : MonoBehaviour
     [Header("Volume")]
     [SerializeField] private GameObject _volume;
 
-    
+
 
     [Space(10)]
     [Header("Controls")]
@@ -71,26 +71,58 @@ public class UIManager : MonoBehaviour
     [SerializeField] private bool fr;
 
     [Space(10)]
-    [Header("Event")] 
+    [Header("Event")]
     [SerializeField] private EventSystem _eventSystem;
 
     private UI controls;
 
-    float score = 0;
+    float score1 = 0;
+    float score2 = 0;
+    float score3 = 0;
     int left = 0;
     int right = 0;
 
-    RawImage soundMark1rend;
-    RawImage soundMark2rend;
-    RawImage soundMark3rend;
-    RawImage soundMark4rend;
-    RawImage soundMark5rend;
+    [SerializeField] private GameObject selector1;
+    [SerializeField] private GameObject selector2;
+    [SerializeField] private GameObject selector3;
 
-    [SerializeField] private GameObject soundMark1;
-    [SerializeField] private GameObject soundMark2;
-    [SerializeField] private GameObject soundMark3;
-    [SerializeField] private GameObject soundMark4;
-    [SerializeField] private GameObject soundMark5;
+
+
+    RawImage soundMark1rend1;
+    RawImage soundMark2rend1;
+    RawImage soundMark3rend1;
+    RawImage soundMark4rend1;
+    RawImage soundMark5rend1;
+
+    RawImage soundMark1rend2;
+    RawImage soundMark2rend2;
+    RawImage soundMark3rend2;
+    RawImage soundMark4rend2;
+    RawImage soundMark5rend2;
+
+    RawImage soundMark1rend3;
+    RawImage soundMark2rend3;
+    RawImage soundMark3rend3;
+    RawImage soundMark4rend3;
+    RawImage soundMark5rend3;
+
+    [SerializeField] private GameObject soundMark11;
+    [SerializeField] private GameObject soundMark21;
+    [SerializeField] private GameObject soundMark31;
+    [SerializeField] private GameObject soundMark41;
+    [SerializeField] private GameObject soundMark51;
+
+    [SerializeField] private GameObject soundMark12;
+    [SerializeField] private GameObject soundMark22;
+    [SerializeField] private GameObject soundMark32;
+    [SerializeField] private GameObject soundMark42;
+    [SerializeField] private GameObject soundMark52;
+
+    [SerializeField] private GameObject soundMark13;
+    [SerializeField] private GameObject soundMark23;
+    [SerializeField] private GameObject soundMark33;
+    [SerializeField] private GameObject soundMark43;
+    [SerializeField] private GameObject soundMark53;
 
 
     [Header("CheckBox")]
@@ -106,7 +138,7 @@ public class UIManager : MonoBehaviour
         controls = new UI();
         controls.Menu.escape.performed += ctx => Pause();
         controls.Menu.back.performed += ctx => Back();
-        
+
 
         pauseMenu.SetActive(false);
         infosMenu.SetActive(false);
@@ -121,7 +153,7 @@ public class UIManager : MonoBehaviour
 
         _controls.SetActive(false);
         _language.SetActive(false);
-        
+
 
         controls.Menu.left.performed += ctx => Left();
         controls.Menu.left.canceled += ctx => StopLeft();
@@ -129,13 +161,28 @@ public class UIManager : MonoBehaviour
         controls.Menu.right.performed += ctx => Right();
         controls.Menu.right.canceled += ctx => StopRight();
 
-        soundMark1rend = soundMark1.GetComponent<RawImage>();
-        soundMark2rend = soundMark2.GetComponent<RawImage>();
-        soundMark3rend = soundMark3.GetComponent<RawImage>();
-        soundMark4rend = soundMark4.GetComponent<RawImage>();
-        soundMark5rend = soundMark5.GetComponent<RawImage>();
+        soundMark1rend1 = soundMark11.GetComponent<RawImage>();
+        soundMark2rend1 = soundMark21.GetComponent<RawImage>();
+        soundMark3rend1 = soundMark31.GetComponent<RawImage>();
+        soundMark4rend1 = soundMark41.GetComponent<RawImage>();
+        soundMark5rend1 = soundMark51.GetComponent<RawImage>();
 
-        score = 50;
+        soundMark1rend2 = soundMark12.GetComponent<RawImage>();
+        soundMark2rend2 = soundMark22.GetComponent<RawImage>();
+        soundMark3rend2 = soundMark32.GetComponent<RawImage>();
+        soundMark4rend2 = soundMark42.GetComponent<RawImage>();
+        soundMark5rend2 = soundMark52.GetComponent<RawImage>();
+
+        soundMark1rend3 = soundMark13.GetComponent<RawImage>();
+        soundMark2rend3 = soundMark23.GetComponent<RawImage>();
+        soundMark3rend3 = soundMark33.GetComponent<RawImage>();
+        soundMark4rend3 = soundMark43.GetComponent<RawImage>();
+        soundMark5rend3 = soundMark53.GetComponent<RawImage>();
+
+        score1 = 50;
+        score2 = 50;
+        score3 = 50;
+
     }
 
     void StopLeft()
@@ -150,24 +197,24 @@ public class UIManager : MonoBehaviour
 
     void Left()
     {
-        Debug.Log("1/2");
 
-        if (_eventSystem.currentSelectedGameObject == soundMark1)
+
+        if (_eventSystem.currentSelectedGameObject == selector1 || _eventSystem.currentSelectedGameObject == selector2 || _eventSystem.currentSelectedGameObject == selector3)
         {
             left = 1;
-            Debug.Log("2/2");
+
         }
     }
     void Right()
     {
-        if (_eventSystem.currentSelectedGameObject == soundMark1)
+        if (_eventSystem.currentSelectedGameObject == selector1 || _eventSystem.currentSelectedGameObject == selector2 || _eventSystem.currentSelectedGameObject == selector3)
         {
             right = 1;
         }
     }
 
     private void Pause()
-    {    
+    {
         if (Time.timeScale != 0f)
         {
             Time.timeScale = 0f;
@@ -225,7 +272,7 @@ public class UIManager : MonoBehaviour
     public void Back()
     {
 
-        if (pauseMenu.activeInHierarchy) 
+        if (pauseMenu.activeInHierarchy)
         {
             Resume();
         }
@@ -237,12 +284,12 @@ public class UIManager : MonoBehaviour
             _eventSystem.SetSelectedGameObject(quitButton);
         }
 
-        
+
         else if (_eventSystem.currentSelectedGameObject == frenchButton || _eventSystem.currentSelectedGameObject == englishButton)
         {
             _eventSystem.SetSelectedGameObject(languageButton);
         }
-        else if (_eventSystem.currentSelectedGameObject == soundMark1)
+        else if (_eventSystem.currentSelectedGameObject == selector1 || _eventSystem.currentSelectedGameObject == selector2 || _eventSystem.currentSelectedGameObject == selector3)
         {
             _eventSystem.SetSelectedGameObject(volumeButton);
         }
@@ -289,12 +336,12 @@ public class UIManager : MonoBehaviour
             _eventSystem.SetSelectedGameObject(infosButton);
         }
     }
-    
+
 
     public void Volume()
     {
-        _eventSystem.SetSelectedGameObject(soundMark1);
-       
+        _eventSystem.SetSelectedGameObject(selector1);
+
     }
     public void Controls()
     {
@@ -404,7 +451,7 @@ public class UIManager : MonoBehaviour
         readable1.gameObject.SetActive(false);
         readable2.gameObject.SetActive(false);
         readable3.gameObject.SetActive(false);
-        readable4.gameObject.SetActive(true); 
+        readable4.gameObject.SetActive(true);
         readable5.gameObject.SetActive(false);
         _eventSystem.SetSelectedGameObject(null);
     }
@@ -447,7 +494,7 @@ public class UIManager : MonoBehaviour
             readableButton5.gameObject.SetActive(true);
         }
 
-        
+
 
         if (_eventSystem.currentSelectedGameObject == volumeButton && !_volume.activeInHierarchy)
         {
@@ -470,83 +517,231 @@ public class UIManager : MonoBehaviour
             _language.SetActive(true);
         }
 
-        if (score < 20)
+        if (score1 < 20)
         {
-            var tempColor1 = soundMark1rend.color;
-            tempColor1.a = score / 20;
-            soundMark1rend.color = tempColor1;
-            soundMark1.SetActive(true);
-
-            soundMark2.SetActive(false);
-            soundMark3.SetActive(false);
-            soundMark4.SetActive(false);
-            soundMark5.SetActive(false);
+            var tempColor = soundMark1rend1.color;
+            tempColor.a = score1 / 20;
+            soundMark1rend1.color = tempColor;
+            soundMark11.SetActive(true);
+            soundMark21.SetActive(false);
+            soundMark31.SetActive(false);
+            soundMark41.SetActive(false);
+            soundMark51.SetActive(false);
         }
-        else if (score < 40)
+        else if (score1 < 40)
         {
-            var tempColor2 = soundMark2rend.color;
-            tempColor2.a = (score - 20) / 20;
-            soundMark2rend.color = tempColor2;
-            soundMark1.SetActive(true);
-            soundMark2.SetActive(true);
-            soundMark3.SetActive(false);
-            soundMark4.SetActive(false);
-            soundMark5.SetActive(false);
-
-        }
-        else if (score < 60)
-        {
-            var tempColor3 = soundMark3rend.color;
-            tempColor3.a = (score - 40) / 20;
-            soundMark3rend.color = tempColor3;
-            soundMark1.SetActive(true);
-            soundMark2.SetActive(true);
-            soundMark3.SetActive(true);
-            soundMark4.SetActive(false);
-            soundMark5.SetActive(false);
+            var tempColor = soundMark2rend1.color;
+            tempColor.a = (score1 - 20) / 20;
+            soundMark2rend1.color = tempColor;
+            soundMark11.SetActive(true);
+            soundMark21.SetActive(true);
+            soundMark31.SetActive(false);
+            soundMark41.SetActive(false);
+            soundMark51.SetActive(false);
 
         }
-        else if (score < 80)
+        else if (score1 < 60)
         {
-            var tempColor4 = soundMark4rend.color;
-            tempColor4.a = (score - 60) / 20;
-            soundMark4rend.color = tempColor4;
-            soundMark1.SetActive(true);
-            soundMark2.SetActive(true);
-            soundMark3.SetActive(true);
-            soundMark4.SetActive(true);
-            soundMark5.SetActive(false);
+            var tempColor = soundMark3rend1.color;
+            tempColor.a = (score1 - 40) / 20;
+            soundMark3rend1.color = tempColor;
+            soundMark11.SetActive(true);
+            soundMark21.SetActive(true);
+            soundMark31.SetActive(true);
+            soundMark41.SetActive(false);
+            soundMark51.SetActive(false);
 
         }
-        else if (score < 100)
+        else if (score1 < 80)
         {
-            var tempColor5 = soundMark5rend.color;
-            tempColor5.a = (score - 80) / 20;
-            soundMark5rend.color = tempColor5;
-            soundMark1.SetActive(true);
-            soundMark2.SetActive(true);
-            soundMark3.SetActive(true);
-            soundMark4.SetActive(true);
-            soundMark5.SetActive(true);
+            var tempColor = soundMark4rend1.color;
+            tempColor.a = (score1 - 60) / 20;
+            soundMark4rend1.color = tempColor;
+            soundMark11.SetActive(true);
+            soundMark21.SetActive(true);
+            soundMark31.SetActive(true);
+            soundMark41.SetActive(true);
+            soundMark51.SetActive(false);
+
+        }
+        else if (score1 < 100)
+        {
+            var tempColor = soundMark5rend1.color;
+            tempColor.a = (score1 - 80) / 20;
+            soundMark5rend1.color = tempColor;
+            soundMark11.SetActive(true);
+            soundMark21.SetActive(true);
+            soundMark31.SetActive(true);
+            soundMark41.SetActive(true);
+            soundMark51.SetActive(true);
 
         }
 
-        if (right == 1 && score < 100)
+        if (score2 < 20)
         {
-            score += 0.5f;
+            var tempColor = soundMark1rend2.color;
+            tempColor.a = score2 / 20;
+            soundMark1rend2.color = tempColor;
+            soundMark12.SetActive(true);
+            soundMark22.SetActive(false);
+            soundMark32.SetActive(false);
+            soundMark42.SetActive(false);
+            soundMark52.SetActive(false);
         }
-        if (left == 1 && score > 0)
+        else if (score2 < 40)
         {
-            score -= 0.5f;
+            var tempColor = soundMark2rend2.color;
+            tempColor.a = (score2 - 20) / 20;
+            soundMark2rend2.color = tempColor;
+            soundMark12.SetActive(true);
+            soundMark22.SetActive(true);
+            soundMark32.SetActive(false);
+            soundMark42.SetActive(false);
+            soundMark52.SetActive(false);
 
         }
+
         //Debug.Log(score);
+
+        else if (score2 < 60)
+        {
+            var tempColor = soundMark3rend2.color;
+            tempColor.a = (score2 - 40) / 20;
+            soundMark3rend2.color = tempColor;
+            soundMark12.SetActive(true);
+            soundMark22.SetActive(true);
+            soundMark32.SetActive(true);
+            soundMark42.SetActive(false);
+            soundMark52.SetActive(false);
+
+        }
+        else if (score2 < 80)
+        {
+            var tempColor = soundMark4rend2.color;
+            tempColor.a = (score2 - 60) / 20;
+            soundMark4rend2.color = tempColor;
+            soundMark12.SetActive(true);
+            soundMark22.SetActive(true);
+            soundMark32.SetActive(true);
+            soundMark42.SetActive(true);
+            soundMark52.SetActive(false);
+
+        }
+        else if (score2 < 100)
+        {
+            var tempColor = soundMark5rend2.color;
+            tempColor.a = (score2 - 80) / 20;
+            soundMark5rend2.color = tempColor;
+            soundMark12.SetActive(true);
+            soundMark22.SetActive(true);
+            soundMark32.SetActive(true);
+            soundMark42.SetActive(true);
+            soundMark52.SetActive(true);
+
+        }
+
+        if (score3 < 20)
+        {
+            var tempColor = soundMark1rend3.color;
+            tempColor.a = score3 / 20;
+            soundMark1rend3.color = tempColor;
+            soundMark13.SetActive(true);
+            soundMark23.SetActive(false);
+            soundMark33.SetActive(false);
+            soundMark43.SetActive(false);
+            soundMark53.SetActive(false);
+        }
+        else if (score3 < 40)
+        {
+            var tempColor = soundMark2rend3.color;
+            tempColor.a = (score3 - 20) / 20;
+            soundMark2rend3.color = tempColor;
+            soundMark13.SetActive(true);
+            soundMark23.SetActive(true);
+            soundMark33.SetActive(false);
+            soundMark43.SetActive(false);
+            soundMark53.SetActive(false);
+
+        }
+        else if (score3 < 60)
+        {
+            var tempColor = soundMark3rend3.color;
+            tempColor.a = (score3 - 40) / 20;
+            soundMark3rend3.color = tempColor;
+            soundMark13.SetActive(true);
+            soundMark23.SetActive(true);
+            soundMark33.SetActive(true);
+            soundMark43.SetActive(false);
+            soundMark53.SetActive(false);
+
+        }
+        else if (score3 < 80)
+        {
+            var tempColor = soundMark4rend3.color;
+            tempColor.a = (score3 - 60) / 20;
+            soundMark4rend3.color = tempColor;
+            soundMark13.SetActive(true);
+            soundMark23.SetActive(true);
+            soundMark33.SetActive(true);
+            soundMark43.SetActive(true);
+            soundMark53.SetActive(false);
+
+        }
+        else if (score3 < 100)
+        {
+            var tempColor = soundMark5rend3.color;
+            tempColor.a = (score3 - 80) / 20;
+            soundMark5rend3.color = tempColor;
+            soundMark13.SetActive(true);
+            soundMark23.SetActive(true);
+            soundMark33.SetActive(true);
+            soundMark43.SetActive(true);
+            soundMark53.SetActive(true);
+
+        }
+
+        if (_eventSystem.currentSelectedGameObject==selector1) 
+        {
+            if (right == 1 && score1 < 100)
+            {
+                score1 += 0.5f;
+            }
+            if (left == 1 && score1 > 0)
+            {
+                score1 -= 0.5f;
+            }
+        }
+
+        if (_eventSystem.currentSelectedGameObject==selector2)
+        {
+            if (right == 1 && score2 < 100)
+            {
+                score2 += 0.5f;
+            }
+            if (left == 1 && score2 > 0)
+            {
+                score2 -= 0.5f;
+            }
+        }
+
+        if (_eventSystem.currentSelectedGameObject==selector3)
+        {
+            if (right == 1 && score3 < 100)
+            {
+                score3 += 0.5f;
+            }
+            if (left == 1 && score3 > 0)
+            {
+                score3 -= 0.5f;
+            }
+        }
+
 
     }
 
     private void FixedUpdate()
     {
-        
+
     }
 
 
@@ -560,6 +755,6 @@ public class UIManager : MonoBehaviour
     }
 
 
-    
+
 
 }
